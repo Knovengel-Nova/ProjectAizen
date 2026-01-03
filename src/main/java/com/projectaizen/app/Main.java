10687faf5c43ff9138c945c04c4053a240497da6
Knovengel <Knovengel@users.noreply.github.com>
2026-01-03 12:58:02 +0530

    trial

--- src/main/java/com/projectaizen/app/Main.java
+++ src/main/java/com/projectaizen/app/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String[] args) {
         Player p1 = new HumanPlayer("Aryan");
         Player p2 = new BotPlayer();
+        
         Match match = new Match();
         
         match.startMatch(p1, p2);
